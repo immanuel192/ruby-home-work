f6557cb187d4ddf7a018e9968ca6822d472bd6ac
Trung Dang <trungdt@absoft.vn>
2020-09-06 22:33:10 +1000

    chore: update test util

--- tests/jest-test-util.ts
+++ tests/jest-test-util.ts
@@ -1,7 +1,8 @@
 // To setup vue local instance for vue-jest
 import Vue from 'vue';
 import VueComposition from '@vue/composition-api';
-import Framework7 from '@/framework7-custom';
+// Do not use the file framework7-custom here
+import Framework7 from 'framework7/js/framework7';
 import Framework7Vue from 'framework7-vue';
 
 Vue.use(VueComposition);
